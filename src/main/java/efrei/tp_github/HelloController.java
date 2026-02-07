fd797d7738d22e6b3e347ef7b3483d8f024b24e0
You2Ss926 <youxssef94200@gmail.com>
2026-02-07 02:25:11 +0100

    Update HelloController.java

--- src/main/java/efrei/tp_github/HelloController.java
+++ src/main/java/efrei/tp_github/HelloController.java
@@ -1,3 +1,5 @@
+package efrei.tp_github;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -5,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/")
-    public String hello() {
+    public String home() {
         return "Hello World Docker";
     }
 }
